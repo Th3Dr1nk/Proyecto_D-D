13433800984de4dc08f2e0724114caf59d05f1b0
TheDrink <danielmarrerocabrrera@gmail.com>
2026-06-01 22:12:54 +0100

    Creacion de Guerrero

--- src/net/SalesianosSchool/Guerrero/Guerrero.java
+++ src/net/SalesianosSchool/Guerrero/Guerrero.java
@@ -1,5 +1,25 @@
 package net.SalesianosSchool.Guerrero;
 
-public class Guerrero {
-    
+import net.SalesianosSchool.Personaje.Personaje;
+
+public class Guerrero extends Personaje {
+    private int extraArmor;
+
+    public Guerrero(String name, int health, int damage, int extraArmor) {
+        super(name, health, damage);
+        this.extraArmor = extraArmor;
+    }
+
+    @Override
+    public void attack(Personaje target) {
+        System.out.println(this.name + " swings a mighty sword at " + target.getName() + "!");
+        target.takeDamage(this.damage);
+    }
+
+    @Override
+    public void takeDamage(int amount) {
+        int reducedDamage = amount - this.extraArmor;
+        if (reducedDamage < 0) reducedDamage = 0;
+        super.takeDamage(reducedDamage);
+    }
 }
