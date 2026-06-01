a00d0355314f9b967833c5b755ffb85dddf9978c
TheDrink <danielmarrerocabrrera@gmail.com>
2026-06-01 22:14:43 +0100

    clase Mago con hechizos implementada

--- src/net/SalesianosSchool/Mago/Mago.java
+++ src/net/SalesianosSchool/Mago/Mago.java
@@ -1,5 +1,25 @@
 package net.SalesianosSchool.Mago;
 
-public class Mago {
-    
+// Importamos la clase base
+import net.SalesianosSchool.Personaje.Personaje;
+
+public class Mago extends Personaje {
+    private int mana;
+
+    public Mago(String name, int health, int damage, int mana) {
+        super(name, health, damage);
+        this.mana = mana;
+    }
+
+    @Override
+    public void attack(Personaje target) {
+        if (this.mana >= 10) {
+            System.out.println(this.name + " lanza una bola de fuego a " + target.getName() + "!");
+            target.takeDamage(this.damage * 2); 
+            this.mana -= 10;
+        } else {
+            System.out.println(this.name + " ataca con su bastón a " + target.getName() + ".");
+            target.takeDamage(this.damage);
+        }
+    }
 }
